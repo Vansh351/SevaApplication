46a06d2aaf1d39d5c7d91eceaf7c2eef78198647
Vansh Chauhan <96383954+Vansh351@users.noreply.github.com>
2023-07-15 06:51:17 -0700

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,5 +1,5 @@
 //const backendURL = "https://crowd-funding-backend.vercel.app/";
- const backendURL = "http://localhost:4000/";
+ const backendURL = process.env.REACT_APP_BASE_URL";
 
 export const getAllCampaignsUrl = () => {
   return backendURL + "api/campaign/all";
